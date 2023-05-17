be56812f21abe91e71eb1a5a29b743c96f2c24fe
†村神† <CA01971172@st.kawahara.ac.jp>
2023-05-17 12:39:08 +0900

    update

--- designSpecification/testFormatConvert2.js
+++ designSpecification/testFormatConvert2.js
@@ -22,36 +22,55 @@ function convertTextToJSON(text) {
             line = lines[i]
         }
         console.log(line,i)
-        if(line.startsWith('# ') || line.startsWith('##')){ //カーソルが# か##で始まる行まで下がった場合
-            // とりあえず発言キャラクターを登録する
-            let name
-            if(line.startsWith('##')){
-                // ##で始まる場合、発言キャラクターを指定しない
-                name = null
-            }else if(line.startsWith('# ')){
-                // # で始まる場合、発言キャラクターを指定する
-                name = line.slice(2)
+        if(line.startsWith('# ') || line.startsWith('##') || line.startsWith('---')){ //カーソルが# か##か---で始まる行まで下がった場合
+            if(line.startsWith('# ') || line.startsWith('##')){ //カーソルが# か##で始まる行まで下がった場合
+                // とりあえず発言キャラクターを登録する
+                let name
+                if(line.startsWith('##')){
+                    // ##で始まる場合、発言キャラクターを指定しない
+                    name = null
+                }else if(line.startsWith('# ')){
+                    // # で始まる場合、発言キャラクターを指定する
+                    name = line.slice(2)
+                }
+                currentData.characterName = name
+                // カーソルが最後の行まで下がったなら、ここで変換処理を終わる
+                if(i === lines.length - 1) break
+                // まだ行が残っているなら、カーソルを1つ下げる
+                i++
+                line = lines[i]
+                // # か##か---か```で始まる行までカーソルを下げる
+                // あるいは、最後の行になるまでカーソルを下げる
+                while(i < lines.length - 1){
+                    if(line.startsWith('# ')) break
+                    if(line.startsWith('##')) break
+                    if(line.startsWith('---')) break
+                    if(line.startsWith('```')) break
+                    i++
+                    line = lines[i]
+                }
+                // カーソルが```で始まる行まで下がる前に他の指示があれば、次のセクションに移動する
+                if(!line.startsWith('```')) break
+                //TODO
+            }else if(line.startsWith('---')){
+                //カーソルが---で始まる行まで下がった場合、区切り線のデータとして登録する
+                currentData.isBorder = true
             }
-            currentData.characterName = name
-            //TODO
+            currentData.characterName = line + ":" + i
+            //作成したデータを登録する
+            chatPalettes.push(currentData)
             if(i === lines.length - 1){
                 // カーソルが最後の行まで下がったなら、変換処理を終わる
                 break
+            }else{
+                // カーソルが最後の行まで下がったなら、ここで変換処理を終わる
+                if(i === lines.length - 1) break
+                // まだ行が残っているなら、カーソルを1つ下げる
+                i++
+                line = lines[i]
             }
-        }else if(line.startsWith('---')){
-            //カーソルが---で始まる行まで下がった場合、区切り線のデータとして登録する
-            currentData.isBorder = true
         }
-        //TODO
-        currentData.characterName = line + ":" + i
-        //作成したデータを登録する
-        chatPalettes.push(currentData)
-        //カーソルを1つ下げる
-        i++
-        line = lines[i]
     }
-
-
     return chatPalettes;
 }
 
