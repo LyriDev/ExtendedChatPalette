0dde974c5bf5e6a79797aba4674d07c7f0bf5146
†村神† <CA01971172@st.kawahara.ac.jp>
2023-05-18 13:42:24 +0900

    update trial code

--- trial/addExButton.js
+++ trial/addExButton.js
@@ -11,7 +11,8 @@ function addExtendedChatPaletteButton(){ // 拡張チャットパレットボタ
         <button class="MuiButtonBase-root MuiIconButton-root sc-bWXABl iZZULD" tabindex="0" type="button">
             <span class="MuiIconButton-label">
                 <svg class="MuiSvgIcon-root" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
-                    <path d="M19 5v14H5V5h14m1.1-2H3.9c-.5 0-.9.4-.9.9v16.2c0 .4.4.9.9.9h16.2c.4 0 .9-.5.9-.9V3.9c0-.5-.5-.9-.9-.9zM11 7h6v2h-6V7zm0 4h6v2h-6v-2zm0 4h6v2h-6zM7 7h2v2H7zm0 4h2v2H7zm0 4h2v2H7z"</path>
+                    <path d="M19 5v14H5V5h14m1.1-2H3.9c-.5 0-.9.4-.9.9v16.2c0 .4.4.9.9.9h16.2c.4 0 .9-.5.9-.9V3.9c0-.5-.5-.9-.9-.9zM11 7h6v2h-6V7zm0 4h6v2h-6v-2zm0 4h6v2h-6zM7 7h2v2H7zm0 4h2v2H7zm0 4h2v2H7z"></path>
+                    <text fill="#f50057" x="17px" y="21px" dominant-baseline="middle" text-anchor="middle" font-size="11px" font-weight="bold">Ex</text>
                 </svg>
             </span>
             <span class="MuiTouchRipple-root"></span>
