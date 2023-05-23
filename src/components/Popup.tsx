31c258c67c570c431906468d46f1e8e84ae570df
†村神† <CA01971172@st.kawahara.ac.jp>
2023-05-23 16:19:46 +0900

    Implement saveable check box

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -2,7 +2,41 @@ import React from 'react';
 import { useState, useEffect } from "react";
 
 function Popup() {
-    const [isChecked, setIsChecked] = useState(true);
+    const [enableExDodge, setEnableExDodge] = useState(true);
+
+    useEffect(() => {
+        chrome.storage.local.get("settings", function(result) {
+            if ((result.settings) && (result.settings.enableExDodge !== undefined)) {
+                // 設定データを取得する
+                setEnableExDodge(result.settings.enableExDodge);
+            }else{
+                // 設定データが存在しない場合は初期化する
+                const defaultSettings: object = {
+                    enableExDodge: enableExDodge
+                };
+                chrome.storage.local.set({ settings: defaultSettings }, function() {
+                    console.log('Settings initialized');
+                });
+            }
+        });
+    }, []);
+
+    // ボタンをクリックしたときの処理
+    const handleButtonClick = () => {
+        const newSettings = {
+            enableExDodge: !enableExDodge
+        };
+
+        // 設定データを更新してストレージに保存する
+        chrome.storage.local.set({ settings: newSettings }, function() {
+            console.log('Settings updated');
+            setEnableExDodge(!enableExDodge);
+        });
+
+        chrome.storage.local.get(function(result) {
+            console.log(result)
+        })
+    };
 
     return (
         <div className="App">
@@ -10,8 +44,8 @@ function Popup() {
                 <input
                     type="checkbox"
                     id="check"
-                    checked={isChecked}
-                    onChange={() => setIsChecked(prevState => !prevState)}
+                    checked={enableExDodge}
+                    onChange={handleButtonClick}
                 />
                 <label htmlFor="check">
                     チェック：
