6504e098d39f347f0355cf8f1ae93d80b0b02cfb
†村神† <CA01971172@st.kawahara.ac.jp>
2023-05-29 12:53:04 +0900

    Implement providers

---
+++ src/providers/TextProvider.tsx
@@ -0,0 +1,19 @@
+import React, { createContext, useState, useEffect } from 'react';
+import { getTexts } from "./../data/DataControl"
+
+export const TextContext = createContext<[string[], React.Dispatch<React.SetStateAction<string[]>>] | null>(null)
+
+export function TextProvider({children}){
+    const [texts, setTexts] = useState<string[]>([]);
+
+    useEffect(() => {
+        const receivedData: string[] = getTexts()
+        setTexts(receivedData)
+    }, []);
+
+    return (
+        <TextContext.Provider value={[texts, setTexts] }>
+            {children}
+        </TextContext.Provider>
+    );
+}
