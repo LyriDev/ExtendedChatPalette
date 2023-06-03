6fa1a55b2d56456b1238b81f13796a8e6f1dd4c5
†村神† <107534447+CA01971172@users.noreply.github.com>
2023-06-03 22:16:19 +0900

    try

--- src/components/App/View/ExChatPaletteView.tsx
+++ src/components/App/View/ExChatPaletteView.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useRef, useContext } from 'react';
 import ReactDOM from 'react-dom';
-import Draggable, { DraggableEvent, DraggableData } from 'react-draggable';
+import Draggable from 'react-draggable';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import { PaletteWindowContext } from "./../../../providers/App/PaletteWindowProvider"
@@ -50,8 +50,11 @@ export default function ExChatPaletteView() {
     const [menuVisible, setMenuVisible, openMenu, closeMenu, toggleMenu] = useContext(PaletteWindowContext) || []; // 拡張チャットパレットが開いているかどうかを管理するコンテキスト
     const resource = useContext(ModalContext); // モーダルメニュー用のコンテキスト
 
-    const menuWidth = 320; // 拡張チャットパレットメニューの横幅
-    const menuHeight = 280; // 拡張チャットパレットメニューの縦幅
+    const menuRef: React.RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null) // メニューのref
+    const defaultWidth: number = 320
+    const defaultHeight: number = 280
+    const menuWidth = menuRef.current?.offsetWidth || defaultWidth; // 拡張チャットパレットメニューの横幅
+    const menuHeight = menuRef.current?.offsetHeight || defaultHeight; // 拡張チャットパレットメニューの縦幅
     const [isDragging, setIsDragging] = useState<boolean>(false); // 拡張チャットパレットをドラッグしているかどうか
 
     const [focusIndex, setFocusIndex] = useState<number>(0); // フォーカスしているタブのindex(View用)
@@ -76,17 +79,19 @@ export default function ExChatPaletteView() {
                     cancel=".draggable-disable"
                     >
                         <div
+                        ref={menuRef}
+                        onClick={()=>console.log("menuWidth:"+menuWidth+"\nmenuHeight"+menuHeight)}
                         style={{
                             position: "absolute",
                             color: "#fff",
                             backgroundColor: 'rgba(44, 44, 44, 0.87)',
                             boxShadow: "0px 6px 6px -3px rgba(0,0,0,0.2),0px 10px 14px 1px rgba(0,0,0,0.14),0px 4px 18px 3px rgba(0,0,0,0.12)",
                             cursor: isDragging ? "grabbing" : "grab",
-                            minWidth: "320px",
-                            minHeight: "280px",
-                            width: "320px",
-                            height: "280px",
-                            // resize: "both"
+                            minWidth: `${defaultWidth}px`,
+                            minHeight: `${defaultHeight}px`,
+                            width: `${defaultWidth}px`,
+                            height: `${defaultHeight}px`,
+                            resize: "both"
                         }}
                         >
                             <ThemeProvider theme={theme}>
