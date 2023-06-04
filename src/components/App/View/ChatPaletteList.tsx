16580eee0944ed08b4373115387e40bd0164ef49
†村神† <107534447+CA01971172@users.noreply.github.com>
2023-06-05 06:07:41 +0900

    Implement data table

--- src/components/App/View/ChatPaletteList.tsx
+++ src/components/App/View/ChatPaletteList.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { DataContext } from "../../../providers/App/DataProvider"
+import { styled } from '@mui/material/styles';
 
 interface TabPanelProps {
     children?: React.ReactNode;
@@ -28,6 +29,19 @@ function TabPanel(props: TabPanelProps) {
     );
 }
 
+const NameTd = styled('td')({
+    boxSizing: "border-box",
+    padding: "8px 16px",
+    border: "solid 1px red",
+    textOverflow: "ellipsis"
+});
+const MessageTd = styled('td')({
+    boxSizing: "border-box",
+    padding: "8px 16px",
+    border: "solid 1px red",
+    whiteSpace: "nowrap"
+});
+
 export default function ChatPaletteList({focusIndex}: {focusIndex: number}) {
     const [chatPalettes, setChatPalettes] = useContext(DataContext) || [];
 
@@ -36,8 +50,8 @@ export default function ChatPaletteList({focusIndex}: {focusIndex: number}) {
     },[])
 
     return (
-        <div
-        className="draggable-disable"
+        <table
+        className="palette-table draggable-disable"
         style={{
             width: "100%",
             height: "calc(100% - 97px)",
@@ -45,14 +59,46 @@ export default function ChatPaletteList({focusIndex}: {focusIndex: number}) {
             cursor: "auto",
         }}
         >
-            {chatPalettes?.map((paletteList, listIndex) => (
-                <TabPanel focusIndex={focusIndex} index={listIndex}>
-                    {paletteList?.map((data, paletteIndex) => (
-                        <div id={"paletteIndex:"+paletteIndex} style={{whiteSpace: "nowrap"}}>{JSON.stringify(data)}</div>
-                        // <div>{data.characterName}:{data.messages}</div>
-                    ))}
-                </TabPanel>
-            ))}
-        </div>
+            <tbody>
+                {chatPalettes?.map((paletteList, listIndex) => (
+                    <TabPanel focusIndex={focusIndex} index={listIndex}>
+                        {paletteList?.map((data, paletteIndex) => (
+                            (data.isBorder) ? 
+                                (
+                                    <tr><td style={{border: "solid 1px blue"}} colSpan={2}></td></tr>
+                                ) : (
+                                    (data.messages.length > 0) ? (
+                                        data.messages?.map((message, messageIndex) => (
+                                            <tr>
+                                                {(messageIndex === 0) && (
+                                                    <NameTd
+                                                    rowSpan={data.messages.length}
+                                                    style={{
+                                                        whiteSpace: ((data.messages.length <= 1) ? "nowrap" : "normal")
+                                                    }}>
+                                                        {data.characterName}
+                                                    </NameTd>
+                                                )}
+                                                <MessageTd>
+                                                    {message}
+                                                </MessageTd>
+                                            </tr>
+                                        ))
+                                    ):(
+                                        <tr>
+                                            <NameTd
+                                            colSpan={2}
+                                            style={{whiteSpace: "nowrap"}}
+                                            >
+                                                {data.characterName}
+                                            </NameTd>
+                                        </tr>
+                                    )
+                                )
+                        ))}
+                    </TabPanel>
+                ))}
+            </tbody>
+        </table>
     );
 }
