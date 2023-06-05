0fb692faed708adbd99c1bfa90cbfa9927ffb170
†村神† <CA01971172@st.kawahara.ac.jp>
2023-06-05 16:06:14 +0900

    introduce Triangle

---
+++ src/svg/Triangle.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Triangle() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+            <path d="M7 10l5 5 5-5z"></path>
+        </svg>
+    );
+}
+
+export default Triangle;
