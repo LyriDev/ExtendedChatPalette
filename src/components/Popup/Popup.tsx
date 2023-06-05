1a6b829ba461176a1c97a36455cd079f36faa9d9
†村神† <107534447+CA01971172@users.noreply.github.com>
2023-06-05 20:59:47 +0900

    delete test code

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -62,27 +62,26 @@ export default function Popup() {
         });
     }
 
-    function getAllData(): void{
+/*     function getAllData(): void{
         chrome.storage.local.get(function(result) {
             console.log(result);
         });
     }
     function clearData(): void{
         chrome.storage.local.clear();
-    }
+    } */
 
     return (
         <div className="App" >
             <ThemeProvider theme={theme}>
-                <button onClick={()=>{deleteData(window.prompt("ルームIDを入力") || "")}}>deleteData</button>
+{/*                 <button onClick={()=>{deleteData(window.prompt("ルームIDを入力") || "")}}>deleteData</button>
                 <button onClick={getAllData}>getData</button>
                 <button onClick={clearData}>clearData</button>
-
                 <button onClick={()=>{
                     chrome.storage.local.getBytesInUse(["data","jCzJmypt5", "tabs"], function(bytesInUse) {
                         console.log("お試し部屋",bytesInUse)
                     });
-                }}>お試し部屋</button>
+                }}>お試し部屋</button> */}
                 <h3>拡張チャットパレット 設定</h3>
                 <div>
                     <input
