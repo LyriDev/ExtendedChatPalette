09c36d93a7073a559a90a7a24a998844d6b5ca69
†村神† <CA01971172@st.kawahara.ac.jp>
2023-06-05 12:34:24 +0900

    Implement hover color

--- src/components/App/View/ChatPaletteList.tsx
+++ src/components/App/View/ChatPaletteList.tsx
@@ -31,31 +31,38 @@ function TabPanel(props: TabPanelProps) {
 
 const NameTd = styled('td')({
     padding: "8px 16px",
-    borderRight: "solid 1px rgba(255, 255, 255, 0.08)",
-    borderBottom: "solid 1px rgba(255, 255, 255, 0.08)"
 });
 const MessageTd = styled('td')({
     padding: "8px 16px",
-    borderBottom: "solid 1px rgba(255, 255, 255, 0.08)",
+    borderBottom: "solid 1px #989898",
     whiteSpace: "nowrap"
 });
 
 export default function ChatPaletteList({focusIndex, width, height}: {focusIndex: number, width:number, height: number}){
     const [chatPalettes, setChatPalettes] = useContext(DataContext) || [];
 
-    const headerHeight: number = 97;
+    const otherHeight: number = 48 + 49;
+
+    const [hoveredRow, setHoveredRow] = useState<number|null>(null);
+    function handleMouseEnter(index: number){
+        setHoveredRow(index);
+    };
+    function handleMouseLeave(){
+        setHoveredRow(null);
+    };
 
     return (
         <div
+        className="draggable-disable"
         style={{
             width: "100%",
-            height: `calc(100% - ${headerHeight}px)`,
+            height: `calc(100% - ${otherHeight}px)`,
             overflow: "auto",
             cursor: "auto",
         }}
         >
             <table
-            className="palette-table draggable-disable"
+            className="palette-table"
             style={{
                 borderCollapse: "collapse"
             }}
@@ -67,46 +74,65 @@ export default function ChatPaletteList({focusIndex, width, height}: {focusIndex
                             {paletteList?.map((data, paletteIndex) => (
                                 (data.isBorder) ? 
                                     (
-                                        <tr><td style={{borderBottom: "solid 1px white"}} colSpan={2}></td></tr>
+                                        <tr><td style={{borderBottom: "solid 2px #fff"}} colSpan={2}></td></tr>
                                     ) : (
-                                        (data.messages.length > 0) ? (
-                                            data.messages?.map((message, messageIndex) => (
-                                                <tr>
-                                                    {(messageIndex === 0) && (
-                                                        <NameTd
-                                                        rowSpan={data.messages.length}
-                                                        >
-                                                        <div
-                                                        style={{
-                                                            width: "3rem",
-                                                            whiteSpace: ((data.messages.length <= 1) ? "nowrap" : "normal"),
-                                                            overflow: "hidden",
-                                                            textOverflow: "ellipsis"
-                                                        }}>
-                                                            {data.characterName}
-                                                        </div>
-                                                        </NameTd>
-                                                    )}
-                                                    <MessageTd>
-                                                        {message}
-                                                    </MessageTd>
-                                                </tr>
-                                            ))
-                                        ):(
-                                            <tr>
-                                                <NameTd
-                                                colSpan={2}
+                                            (data.messages.length > 0) ? (
+                                                data.messages?.map((message, messageIndex) => (
+                                                    <tr
+                                                    style={{
+                                                        backgroundColor: ((hoveredRow === paletteIndex) ? "rgba(255, 255, 255, 0.08)" : ""),
+                                                        cursor: "pointer",
+                                                    }}
+                                                    onMouseEnter={() => handleMouseEnter(paletteIndex)}
+                                                    onMouseLeave={handleMouseLeave}
+                                                    >
+                                                        {(messageIndex === 0) && (
+                                                            <NameTd
+                                                            rowSpan={data.messages.length}
+                                                            style={{
+                                                                borderRight: "solid 1px #989898",
+                                                                borderBottom: ((paletteList[paletteIndex+1]?.isBorder) ? "none" : "solid 1px #989898")
+                                                            }}
+                                                            >
+                                                            <div
+                                                            style={{
+                                                                width: "3rem",
+                                                                whiteSpace: ((data.messages.length <= 1) ? "nowrap" : "normal"),
+                                                                overflow: "hidden",
+                                                                textOverflow: "ellipsis"
+                                                            }}>
+                                                                {data.characterName}
+                                                            </div>
+                                                            </NameTd>
+                                                        )}
+                                                        <MessageTd>
+                                                            {message}
+                                                        </MessageTd>
+                                                    </tr>
+                                                ))
+                                            ):(
+                                                <tr
                                                 style={{
-                                                    maxWidth: "0",
-                                                    whiteSpace: "nowrap",
-                                                    overflow: "hidden",
-                                                    textOverflow: "ellipsis",
+                                                    backgroundColor: ((hoveredRow === paletteIndex) ? "rgba(255, 255, 255, 0.08)" : ""),
+                                                    cursor: "pointer",
                                                 }}
+                                                onMouseEnter={() => handleMouseEnter(paletteIndex)}
+                                                onMouseLeave={handleMouseLeave}
                                                 >
-                                                    {data.characterName}
-                                                </NameTd>
-                                            </tr>
-                                        )
+                                                    <NameTd
+                                                    colSpan={2}
+                                                    style={{
+                                                        maxWidth: "0",
+                                                        whiteSpace: "nowrap",
+                                                        overflow: "hidden",
+                                                        textOverflow: "ellipsis",
+                                                        borderBottom: ((paletteList[paletteIndex+1]?.isBorder) ? "none" : "solid 1px #989898")
+                                                    }}
+                                                    >
+                                                        {data.characterName}
+                                                    </NameTd>
+                                                </tr>
+                                            )
                                     )
                             ))}
                         </TabPanel>
