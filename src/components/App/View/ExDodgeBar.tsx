07831f5bb43d940022f265e0607e6e2d7a17c0bf
†村神† <107534447+CA01971172@users.noreply.github.com>
2023-06-07 02:53:07 +0900

    Implement enable control ExDodge count

--- src/components/App/View/ExDodgeBar.tsx
+++ src/components/App/View/ExDodgeBar.tsx
@@ -1,29 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Button from '@mui/material/Button';
 import { changeMessage, clickSubmitButton, getDiceSystem } from "./../../../data/sendCcfoliaMessage"
+import InputNumber from "./InputNumber"
 
 export default function ExDodgeBar() {
     const [dodgeCount, setDodgeCount] = useState<number>(0);
+    const barRef = useRef<HTMLDivElement>(null);
+    const contentRef = useRef<HTMLDivElement>(null);
+
+    // 拡張回避バーが収まっているかどうかを取得する関数
+    function getIsFit(): boolean{
+        let result = true;
+        // 拡張回避バーの横幅を取得する
+        const barRect = barRef.current?.getBoundingClientRect();
+        const barWidth: number = barRect?.width || 0;
+        // 拡張回避バーの中身の横幅を取得する
+        const contentRect = contentRef.current?.getBoundingClientRect();
+        const contentWidth: number = contentRect?.width || 0;
+        // 収まっているかを計算して返す
+        if(contentWidth >= barWidth){
+            result = false;
+        }
+        return result;
+    }
 
     return (
             <div
+            ref={barRef}
             style={{
                 position: "relative",
                 width: "100%",
                 height: "48px",
                 boxShadow: "0px -2px 4px -1px rgba(0,0,0,0.2), 0px -4px 5px 0px rgba(0,0,0,0.14), 0px -1px 10px 0px rgba(0,0,0,0.2)",
+                overflow: "hidden",
             }}
             >
                 <div
+                ref={contentRef}
                 style={{
+                    width: "fit-content",
                     display: "flex",
                     alignItems: "center",
                     position: "absolute",
                     top: "50%",
-                    left: "50%",
-                    transform: "translateY(-50%) translateX(-50%)",
-                    overflow: "hidden",
-                }}>
+                    left: ((getIsFit()) ? "50%" : "0"),
+                    transform: `translateY(-50%) translateX(${((getIsFit()) ? "-50%" : "0")})`,
+                }}
+                >
                     <Button
                     className="draggable-disable"
                     color="primary"
@@ -38,7 +61,7 @@ export default function ExDodgeBar() {
                             const isChangedMessage: boolean = changeMessage(`${diceSystem}<=(({回避技能})/${dodgeCount+1}) 【回避】${dodgeCount+1}回目`);
                             if(!isChangedMessage){
                                 clickSubmitButton();
-                                setDodgeCount((prev) => prev + 1);
+                                setDodgeCount((prev) => (Math.min((prev + 1), 999)));
                             }
                             resolve0();
                         }).then(() =>{
@@ -55,7 +78,7 @@ export default function ExDodgeBar() {
                         cursor: "default",
                     }}
                     >
-                        回避回数:{dodgeCount}
+                        回避回数:<InputNumber value={dodgeCount} setNumber={setDodgeCount} />
                     </div>
                     <Button
                     className="draggable-disable"
---
+++ src/components/App/View/InputNumber.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+type Props = {
+  value: number;
+  setNumber:  React.Dispatch<React.SetStateAction<number>>;
+};
+
+export default function InputNumber({ value, setNumber }: Props){
+  const onChangeHandler = (value: string) => {
+    const v = value.replace(/[０-９．]/g, (s) =>
+      String.fromCharCode(s.charCodeAt(0) - 0xfee0)
+    );
+    if (!isNaN(Number(v))) {
+      setNumber(Math.min(Number(v), 999));
+    }
+  };
+
+  return (
+    <input
+      type="text"
+      inputMode="numeric"
+      value={value}
+      onChange={(e) => onChangeHandler(e.target.value)}
+      style={{
+        width: "2rem",
+        fontSize: "1rem",
+        color: "#fff",
+        margin: "0",
+        marginLeft: "5px",
+        padding: "0",
+        background: "none",
+        border: "none",
+        borderRadius: "0",
+        outline: "none",
+        appearance: "none",
+      }}
+    />
+  );
+};
