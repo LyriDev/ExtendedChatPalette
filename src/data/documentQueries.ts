fd163002a6c71797a7c80d70a38807e6cf11a23d
†村神† <107534447+CA01971172@users.noreply.github.com>
2023-07-10 01:14:36 +0900

    use Index instead of unique id

--- src/data/documentQueries.ts
+++ src/data/documentQueries.ts
@@ -1,32 +1,34 @@
 /* ココフォリアページの要素のクエリの文字列 */
 
 // 画面上部に表示されている「マイキャラクター一覧」のアイコンボタン
-export const myCharacterButtonQuery: string = "#root > div > header > div > button.MuiButtonBase-root.MuiIconButton-root.sc-afnQL.iMcZrZ";
+export const myCharacterButtonQuery: string = "#root > div > header > div > button:nth-of-type(2)"; // ヘッダー内の左から3つ目のボタン
 
 // レスポンシブデザイン用のハンバーガーメニュー開閉ボタン
-export const hamburgerMenuButtonQuery: string = "#root > div > header > div > button.MuiButtonBase-root.MuiIconButton-root.sc-kngDgl.kExRru";
+export const hamburgerMenuButtonQuery: string = "#root > div > header > div > button:nth-of-type(8)";
 
 // レスポンシブデザイン用のul要素内の、一番上の要素である「マイキャラクター」欄(であると予測される要素)を取得する
 export const myCharacterColumnQuery: string = "body > div.MuiPopover-root > div.MuiPaper-root.MuiPopover-paper.MuiPaper-elevation8.MuiPaper-rounded > ul > li:first-child";
 
-// キャラクター編集メニューの一番上の見出しである「キャラクター編集」
+// キャラクター編集メニューの一番上の「キャラクター編集」見出し
 export const editHeaderQuery: string = "body > div.MuiDialog-root > div.MuiDialog-container.MuiDialog-scrollPaper > div > header > div > p";
 
-// メッセージ入力フォームの、名前欄
-export const nameFormQuery: string = "#root > div > div.MuiDrawer-root.MuiDrawer-docked.sc-hsOonA.buVWki > div > div > div > form > div.sc-jGprRt.cTnqPF > div.sc-hAsxaJ.dggwKE > div > input";
+// 入力フォーム
+const formQuery: string = "#root > div > div:nth-child(5) > div > div > div > form";
 
-// メッセージ入力フォームの、メッセージ欄
-export const messageFormQuery: string = "#downshift-0-input";
+// メッセージ入力フォームの、名前欄
+export const nameFormQuery: string = `${formQuery} > div:nth-child(2) > div:nth-child(1) > div > input`;
 
 // メッセージ入力フォームの、送信ボタン
-export const submitFormQuery: string = "#root > div > div.MuiDrawer-root.MuiDrawer-docked.sc-hsOonA.buVWki > div > div > div > form > div.sc-jGprRt.cTnqPF > button.MuiButtonBase-root.MuiButton-root.MuiButton-text.MuiButton-textSizeSmall.MuiButton-sizeSmall";
+export const submitFormQuery: string = `${formQuery} > div:nth-child(2) > button:nth-of-type(3)`;
+
+// メッセージ入力フォームの、メッセージ欄
+export const messageFormQuery: string = `${formQuery} > div:nth-child(3) textarea`;
 
 // 「ルームチャット」タブ(メッセージ一覧)
 export const roomChatQuery: string = "#root > div > div.MuiDrawer-root.MuiDrawer-docked.sc-hsOonA.buVWki > div > div > ul > div:nth-child(1) > div";
 export const messageColumnQuery: string = `${roomChatQuery} > div`;
 
-// 画面左上の、ダイスシステム欄
-export const diceSystemQuery: string = "#root > div > header > div > button:nth-child(1) > span.MuiButton-label > h6 > span";
-
 // 画面左上の、ルーム名欄
 export const roomNameQuery: string = "#root > div > header > div > button:nth-child(1) > span.MuiButton-label > h6";
+// 画面左上の、ダイスシステム欄
+export const diceSystemQuery: string = `${roomNameQuery} > span`;
